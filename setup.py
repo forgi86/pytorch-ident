7e4f19150853c51fa7a639f32546d41620992b29
forgi86 <marco.forgione1986@gmail.com>
2021-05-31 13:31:25 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='torchid',
+    version='0.1',
+    url='https://github.com/forgi86/pytorch-ident.git',
+    author='Marco Forgione',
+    author_email='marco.forgione1986@gmail.com',
+    description='System identification with pytorch package',
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'matplotlib', 'torch'],  # to be checked
+    extras_require={
+        'download datasets': ["requests"],
+        'open datasets': ["pandas"]
+    }
+)
